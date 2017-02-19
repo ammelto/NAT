d3ddc8e30533d14c7978f70e33d7478523ea0dfa
ammelto <alexander.melton@okstate.edu>
2017-02-18 18:46:54 -0600

    initial commit

---
+++ Umbrella/app/src/main/java/com/nerdery/umbrella/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nerdery.umbrella.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.nerdery.umbrella.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
